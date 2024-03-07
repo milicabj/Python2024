7936a538d5669c7bfd4ad5a884e18f841afc5a4f
Milica Bjelica <bjelicamilica1212@gmail.com>
2024-03-07 01:25:46 +0100

    one more task

--- domaci2.py
+++ domaci2.py
@@ -670,4 +670,11 @@ def jednocifreni_negativni(s):
 
 print(jednocifreni_negativni("2+3-2-32+4-22-44"))
 
+'''59. Napisati program koji za unijeti string s (karakteri stringa cifre od 0 do 9) enkriptuje na
+sledeći način: ako je karakter paran broj pretvara se u 0, a ako je karakter neparan broj
+pretvara se u 1. Npr. za s = ‘15023’ rezultat je 11001. Pomoć: Inicijalna vrijednost za
+dodatni string je “”, a onda se pomoću operatora + nadodaje 0 ili 1 u zavisnosti u
+ispunjenosti uslova.
+
+
         
